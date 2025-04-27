c374b812555e3f7307ca7080d93c800e89602fef
mail@mail.com <mail@mail.com>
2025-04-27 19:24:08 +0200

    inlämning

--- src/script.js
+++ src/script.js
@@ -26,22 +26,17 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
-  
-
-
+  let groupToShow = "vänner";
 
 function showContacts(contactBook, groupToShow) { // Rör ej denna kod
 
-
-
     // Loopar igenom alla kontakter och jämför grupp
-   
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
-
-
+    for(contact of contactBook.contacts) {
+      if(contact.group === groupToShow) {
+        return `Namn: ${contact.name}, Telefon: ${contact.phone}`;  // här ska sträng med namn och telefonnummer visas i form av string interpolation
+      }
+    }
 }
-
 
 // Rör ej denna kod
 showContacts(contactBook, groupToShow);
